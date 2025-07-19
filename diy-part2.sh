fa0808e307b60bc6d083151b146b7e146f144bc1
weicaixian86 <118384041+weicaixian86@users.noreply.github.com>
2025-07-19 12:22:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,3 +29,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#tty 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
